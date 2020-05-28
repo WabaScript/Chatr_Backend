68db01704ccde8726e35c3e1cbfed0bab2e46b2d
Babyyodabc <rjwahba@gmail.com>
2020-05-28 14:39:12 -0400

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,6 @@ Rails.application.routes.draw do
   get "/auto_login", to: "auth#auto_login"
   
   mount ActionCable.server => '/cable'
-
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
