e590405dce504986afe46bd7e12cd34600f627c7
Babyyodabc <rjwahba@gmail.com>
2020-05-29 08:21:43 -0400

    config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,11 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  # actioncable acceptances
+  config.action_cable.allowed_request_origins = [
+  'http://localhost:3001',
+  'https://chatr2020.herokuapp.com/'
+  ]
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,11 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+   # actioncable acceptances
+   config.action_cable.allowed_request_origins = [
+    'http://localhost:3001',
+    'https://chatr2020.herokuapp.com/'
+    ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
